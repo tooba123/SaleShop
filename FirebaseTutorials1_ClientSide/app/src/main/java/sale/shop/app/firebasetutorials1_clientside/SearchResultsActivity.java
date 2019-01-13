8f1ad355964d62d25592e3bbd27dcdb34c48318c
tooba123 <2ba.leo@gmail.com>
2019-01-13 17:53:48 +0500

    add new changes in forked repo

---
+++ FirebaseTutorials1_ClientSide/app/src/main/java/sale/shop/app/firebasetutorials1_clientside/SearchResultsActivity.java
@@ -0,0 +1,83 @@
+package sale.shop.app.firebasetutorials1_clientside;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SearchView;
+
+public class SearchResultsActivity extends AppCompatActivity {
+
+
+    SearchView mSearchView;
+
+    public  String query;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        handleIntent(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+
+            query = intent.getStringExtra(SearchManager.QUERY);
+
+//            mSearchView.clearFocus();
+            //use the query to search your data somehow
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+        mSearchView = searchView;
+
+        menu.findItem(R.id.search).expandActionView();
+        mSearchView.setIconifiedByDefault(false);
+        mSearchView.setIconified(false);
+        mSearchView.setQuery(query,false);
+        mSearchView.clearFocus();
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.search) {
+//            mSearchView.setIconifiedByDefault(false);
+//            mSearchView.clearFocus();
+            mSearchView.setIconifiedByDefault(true);
+            mSearchView.setFocusable(true);
+            mSearchView.setIconified(false);
+            mSearchView.requestFocusFromTouch();
+            mSearchView.setQuery(query,false);
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
